083a68bc64b1b5ffcd64037fe63826254532d92d
Mahsun EMREM <mahsun.emrem@W10N-MAHSUN>
2024-05-03 23:48:30 +0300

    Add project files.

---
+++ DockerApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DockerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            while(i < 1_000)
+            {
+                Console.WriteLine($"Hello, World! ({i})");
+
+                i++;
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
